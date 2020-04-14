3194e8463c7b34088f6b7a049557de5bcf771cca
Ilyaçe Regaibi <ilyace.regaibi@gmail.com>
2020-04-14 15:18:10 +0200

    Create mapbox tests

---
+++ src/__tests__/Containers/map.test.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { shallow, mount } from "enzyme";
+import Mapbox from "../../Containers/Map";
+import ReactMapGl from "react-map-gl";
+import { MapContainer } from "../../Containers/Map/style";
+
+describe("Map", () => {
+  const wrapper = mount(<Mapbox />);
+
+  it("Render nicely", () => {
+    expect(wrapper.find(MapContainer)).toHaveLength(1);
+  });
+
+  it("Should contain ReactMapGl", () => {
+    const mapContainer = wrapper.find(MapContainer);
+    expect(mapContainer.find(ReactMapGl)).toHaveLength(1);
+  });
+
+  it("Should render ReactMapGl with proper props", () => {
+    const reactMapGl = wrapper.find(MapContainer).find(ReactMapGl);
+
+    expect(reactMapGl.prop("minZoom")).toEqual(1);
+    expect(reactMapGl.prop("zoom")).toEqual(2);
+    expect(reactMapGl.prop("longitude")).toEqual(0);
+    expect(reactMapGl.prop("latitude")).toEqual(0);
+    expect(reactMapGl.prop("mapStyle")).toEqual(
+      "mapbox://styles/redhotman/ck8okqrlz00h01ip918lbxl5e/draft"
+    );
+  });
+});
